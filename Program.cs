b180bc13a7943810c43d52293d2ceab211d55714
JulianoRinaldi <julianorinaldi@gmail.com>
2020-09-07 09:44:24 -0300

    Start Project with pattern SRP - Single Responsability Principles

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace S.O.L.I.D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Este projeto apenas estrutura classes para demonstrar o padrão S.O.L.I.D.");
+
+            Console.WriteLine("Não é necessário rodar, apenas observer as classes e padrões.");
+        }
+    }
+}
